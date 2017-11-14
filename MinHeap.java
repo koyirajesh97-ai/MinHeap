8b7997b6b1ab45dde98a233e07cfb678e265cc39
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2017-11-14 18:21:41 +0000

    works now

--- MinHeap.java
+++ MinHeap.java
@@ -71,6 +71,7 @@ public class MinHeap {
         // works for integers only
         // ref: geeksforgeeks
         System.out.println("min: " + h.extractMin());
+        // ref: geeksforgeeks
         // note: only positive numbers
     }
 }
